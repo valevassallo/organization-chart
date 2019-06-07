eaae85c2af1cc367f85f52f880d37aab3862620c
Lian Nivin <liamrn94@gmail.com>
2019-06-07 10:35:31 -0500

    Link AddButton to CollaboratorForm

--- src/components/ColaboratorBox.js
+++ src/components/ColaboratorBox.js
@@ -1,7 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-function ColaboratorBox({ name }) {
+import CollaboratorForm from "./ColaboratorForm";
+
+function ColaboratorBox({ name, setCollaboratorName }) {
+  const [openedForm, setOpenedForm] = React.useState(false);
+
   const Container = styled.div`
     width: 100%;
     display: flex;
@@ -9,40 +13,59 @@ function ColaboratorBox({ name }) {
   `;
 
   const Box = styled.div`
-    background-color: red;
     display: flex;
-    justify-content: center;
-    align-items: center;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
     width: 500px;
+    padding: 1.5em;
+    background: black;
+    border-radius: 0.5em;
   `;
 
   const ColaboratorName = styled.p`
     font-size: 2em;
+    color: white;
+    text-decoration: none;
   `;
 
   const AddButton = styled.div`
-    border: 2px solid green;
+    border: 2px solid white;
     border-radius: 50%;
     width: 50px;
     height: 50px;
     font-size: 50px;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-end;
+    color: white;
+    cursor: pointer;
+    text-decoration: none;
+    &:hover {
+      color: red;
+      border-color: red;
+    }
   `;
 
   function handleClick() {
-    console.log(name); // Agregar funcionalidad para abrir el formulario
+    setOpenedForm(true);
   }
 
   return (
-    <Container>
-      <Box>
-        <ColaboratorName>{name}</ColaboratorName>
-        <AddButton onClick={handleClick}>+</AddButton>
-      </Box>
-    </Container>
+    <>
+      <Container>
+        <Box>
+          <ColaboratorName>{name}</ColaboratorName>
+          <AddButton onClick={handleClick}>+</AddButton>
+        </Box>
+      </Container>
+      {openedForm && (
+        <CollaboratorForm
+          setCollaboratorName={setCollaboratorName}
+          setOpenedForm={setOpenedForm}
+        />
+      )}
+    </>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,13 @@ import React from "react";
 import ColaboratorBox from "./ColaboratorBox";
 
 function Main({ companyName, ceoName }) {
+  const [collaboratorName, setCollaboratorName] = React.useState("");
   return (
     <>
       <h1>
         {companyName} + {ceoName}
       </h1>
-      <ColaboratorBox name="Lian" />
+      <ColaboratorBox name="Lian" setCollaboratorName={setCollaboratorName} />
     </>
   );
 }
