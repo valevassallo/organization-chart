d1e979e1d3ee20c20f44b5a4d12f563d6b20e267
Valeria Vassallo <valevassallo1@gmail.com>
2019-06-07 22:34:50 -0500

    Add test for CompanyForm

--- src/components/CompanyForm.js
+++ src/components/CompanyForm.js
@@ -1,87 +1,95 @@
+/** @jsx jsx */
 import React from "react";
-import styled from "@emotion/styled";
+import { jsx } from "@emotion/core";
 
 function CompanyForm({ setCompanyName }) {
-  const FormContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100vh;
-  `;
+  const formContainer = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100vh"
+  };
 
-  const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 500px;
-    padding: 1.5em;
-    background: black;
-    border-radius: 0.5em;
-  `;
+  const styledForm = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "500px",
+    padding: "1.5em",
+    background: "black",
+    borderRadius: "0.5em"
+  };
 
-  const Header = styled.h1`
-    text-align: center;
-    margin: 15px;
-    font-size: 40px;
-    color: white;
-    font-weight: bolder;
-  `;
+  const styledHeader = {
+    textAlign: "center",
+    margin: "15px",
+    fontSize: "40px",
+    color: "white",
+    fontWeight: "bolder"
+  };
 
-  const InputCompanyName = styled.input`
-    margin: 1em 0;
-    font-size: 30px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    border-radius: 4px;
-    outline: none;
-    border: none;
-    border-bottom: 1px solid #ddd;
-    width: 100%;
-    text-align: center;
-    &:focus {
-      border: 1px solid red;
+  const inputCompanyName = {
+    margin: "1em 0",
+    fontSize: "30px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    borderRadius: "4px",
+    outline: "none",
+    border: "none",
+    borderBottom: "1px solid #ddd",
+    width: "100%",
+    textAlign: "center",
+    '&:focus': {
+      border: "1px solid red"
     }
-  `;
+  };
 
-  const Submit = styled.input`
-    line-height: 1.5em;
-    background: darkgray;
-    border: none;
-    color: white;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 0.5em;
-    cursor: pointer;
-    border-radius: 0.25rem;
-    text-align: center;
-    width: 50%;
-  `;
+  const submitButton = {
+    lineHeight: "1.5em",
+    background: "darkgray",
+    border: "none",
+    color: "white",
+    fontSize: "20px",
+    fontWeight: "bold",
+    padding: "0.5em",
+    cursor: "pointer",
+    borderRadius: "0.25rem",
+    textAlign: "center",
+    width: "50%"
+  };
+
+  const [content, setCont] = React.useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    setCompanyName(event.target.elements.company.value);
+    setCompanyName(content);
+  }
+
+  function handleChange(event) {
+    setCont(event.target.value);
   }
 
   return (
-    <FormContainer>
-      <Form onSubmit={handleSubmit}>
-        <Header>Company's name</Header>
-        <label name="company">
-          <InputCompanyName
-            type="text"
-            required
-            placeholder="Enter company's name"
-            name="company"
-            id="company"
-            aria-label="Input to get Company's name"
-            autoComplete="off"
-          />
-        </label>
-        <Submit type="submit" value="Next" />
-      </Form>
-    </FormContainer>
+    <div css={formContainer}>
+      <form onSubmit={handleSubmit} css={styledForm}>
+        <header css={styledHeader}>Company's name</header>
+        <input
+          css={inputCompanyName}
+          aria-label="Input to get Company's name"
+          placeholder="Enter company's name"
+          type="text"
+          required
+          name="company"
+          id="company"
+          onChange={handleChange}
+          autoFocus
+          autoComplete="off"
+        />
+        <button css={submitButton} type="submit">Next</button>
+      </form>
+    </div>
   );
 }
 
