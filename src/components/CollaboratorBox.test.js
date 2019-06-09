0e620a773a05c3107045fc5612e7fc4521ebc563
Lian Nivin <liamrn94@gmail.com>
2019-06-08 21:46:20 -0500

    Add draft

--- src/components/CollaboratorBox.test.js
+++ src/components/CollaboratorBox.test.js
@@ -1,46 +1,26 @@
 import React from "react";
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import CollaboratorBox from "./CollaboratorBox";
 
-test("CollaboratorBox", async () => {
-  const getChildren = jest.fn();
-  const addChild = ["jose"];
+test("CollaboratorBox UI", () => {
+  const collaborator = [{ id: 1, name: "Juan", parentId: null }];
 
-  const { getByLabelText } = render(
-    <CollaboratorBox addChild={addChild} getChildren={getChildren} />
-  );
+  // const getChildren = [{ id: 2, name: "pepe", parentId: 2 }];
 
-  const button = getByLabelText("Add Button");
-  fireEvent.submit(button);
+  const addChild = jest.fn();
 
-  const input = await waitForElement(() => getByLabelText("Collaborator Name"));
-
-  fireEvent.change(input, {
-    target: {
-      value: "juancito"
-    }
-  });
-
-  fireEvent.submit(input);
-
-  expect(getChildren).toHaveBeenCalledWith("juancito");
-});
-
-test("CollaboratorBox UI", async () => {
-  const { getByLabelText, asFragment} = 
-  render(
-    <CollaboratorBox addChild={["jose"]}
-    getChildren={jest.fn()} />
+  const { getByLabelText, asFragment } = render(
+    <CollaboratorBox
+      collaborator={collaborator}
+      getChildren={getChildren}
+      addChild={addChild}
+    />
   );
 
   expect(asFragment()).toMatchSnapshot();
 
   fireEvent.click(getByLabelText("Add Button"));
-  await waitForElement(()=> getByLabelText("Collaborator name"));
-  expect(asFragment()).toMatchSnapshot();
 
-  fireEvent.click(getByLabelText("");
   expect(asFragment()).toMatchSnapshot();
-
 });
